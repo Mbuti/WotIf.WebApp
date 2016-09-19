920f069803b701ba1d0cc5b57e63f5c1700138e9
David Kroukamp <david.kroukamp@dariel.co.za>
2016-09-19 08:52:26 +0200

    Made BI a bit bigger :P

--- src/js/pages/BusinessIntelligence.js
+++ src/js/pages/BusinessIntelligence.js
@@ -6,8 +6,8 @@ export default class BusinessIntelligence extends React.Component {
 			<div>
 				<h1>Business Intelligence</h1>
 				<iframe 
-					width="800"
-					height="600" 
+					width="1000"
+					height="800" 
 					src="https://app.powerbi.com/view?r=eyJrIjoiOGI1N2Q4NmEtZDkyMS00MjY2LWFhZjEtZjQ1ODliODUwZmI4IiwidCI6Ijg3MjE3YzYwLTE1OWEtNDQxOS05MjhmLTVkNWNjMTk5ZDE1OCIsImMiOjh9"
 					frameBorder="0"
 					allowFullScreen="true">
