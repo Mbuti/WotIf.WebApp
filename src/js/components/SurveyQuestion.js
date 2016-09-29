afb38cd94a1a76c84c61481ceaf03c12cbeb0574
David Kroukamp <david.kroukamp@dariel.co.za>
2016-09-29 13:34:55 +0200

    Slight change to text questions. Added client.min.js to gitignore

--- src/js/components/SurveyQuestion.js
+++ src/js/components/SurveyQuestion.js
@@ -47,7 +47,7 @@ export default class SurveyQuestion extends React.Component {
 						<label for={this.props.questionId}>{this.props.text}</label>
 						<div class="input-group">
 							<span class="input-group-addon">R</span>
-							<input type="number" id={this.props.questionId} step="0.01" onChange={this.props.handleChange.bind(this, this.props.questionId)} class="form-control" required />
+							<input type="number" id={this.props.questionId} step="0.05" onChange={this.props.handleChange.bind(this, this.props.questionId)} class="form-control" required />
 						</div>
 					</div>
 				)
@@ -57,10 +57,18 @@ export default class SurveyQuestion extends React.Component {
 				return (
 					<div class="form-group">
 						<label for={this.props.questionId}>{this.props.text}</label>
-						<textarea id={this.props.questionId} onChange={this.props.handleChange.bind(this, this.props.questionId)} class="form-control" required />
+						<input type="text" id={this.props.questionId} onChange={this.props.handleChange.bind(this, this.props.questionId)} class="form-control" required />
 					</div>
 				)
 				break;
+
+			case "LongText":
+				return (
+					<div class="form-group">
+						<label for={this.props.questionId}>{this.props.text}</label>
+						<textarea id={this.props.questionId} onChange={this.props.handleChange.bind(this, this.props.questionId)} class="form-control" required />
+					</div>
+				)	
 
 		}
 		return(<p>Unsupported question type: {this.props.type}</p>);
