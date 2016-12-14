87014594b6269bcdbd93cf016b8a8546d8b55b93
Joshua Selvan <joshua.selvan@headoffice.dariel-solutions>
2016-12-14 17:36:43 +0200

    Trello Requirements met for create-individual page

--- src/app/individuals/create-individual.component.ts
+++ src/app/individuals/create-individual.component.ts
@@ -6,7 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: []
 })
 export class CreateIndividualComponent implements OnInit {
+   show = "yes";
 
+   inputtedValues ={
+     SearchByName:"",
+     Name:"",
+     Surname:"",
+     Age:"",
+     ID:"",
+     Nationality:"",
+     NameOfTalent:"",
+     YearsExperience:"",
+     EntityName:""
+   };
+
+   angular = false;
+   hasTalent = false;
+   hasParticipant = false;
+
+   SubmitData(){
+     alert('IMPLEMENTED this has not BEEN');
+  }  
+
+  SubmitFieldData(stringi){
+    alert(stringi);
+  }
   constructor() { }
 
   ngOnInit() {
